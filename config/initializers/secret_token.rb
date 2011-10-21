948d6bf4d80a5973da0d2123888014d5035bf0e0
Ryan Carter <ry@fabld.com>
2011-10-21 10:28:04 -0600

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '86f7b7cd9c327219a388568f0ae07f641ffc85b4cadc382af5d5efa9b95478c19461511882d71cb6ef030d588beb469cc66ebd72c25af920043efff158984adc'
